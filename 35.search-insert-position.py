fae6818e9e0647ee607617a09bcdf7bb22a64c55
Mufaddal Motiwala <mmotiwala@tesla.com>
2023-05-06 21:13:19 -0700

    p35

---
+++ 35.search-insert-position.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=35 lang=python
+#
+# [35] Search Insert Position
+#
+
+# @lc code=start
+class Solution(object):
+    def searchInsert(self, nums, target):
+        l = 0
+        r = len(nums)-1
+        while l <= r:
+            m = (l+r)//2
+            print("before: ", l, m, r)
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            if nums[m] > target:
+                r = m - 1
+            print("after: ", l, m, r)
+        
+        return (m if nums[m] > target else m+1)
+        
+
+        
+# @lc code=end
+
