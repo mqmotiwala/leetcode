035f2531ee8de947f7fb47b6f6f851015bde18b8
mqmotiwala <mqmotiwala@gmail.com>
2023-05-08 22:20:31 -0700

    p704

---
+++ 704.binary-search.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=704 lang=python
+#
+# [704] Binary Search
+#
+
+# @lc code=start
+class Solution(object):
+    def search(self, nums, target):
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            m = (l+r)//2
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        
+        return -1
+        
+# @lc code=end
+
